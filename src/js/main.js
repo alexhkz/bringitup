dcf0024cdf3311ad5e4ec3707f75a0f5c33f9959
alexhkz <alexwebcode@gmail.com>
2022-04-10 14:32:39 +0300

    add videoplayers

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import MiniSlider from "./modules/slider/slider-mini";
 import VideoPlayer from "./modules/playVideo";
 import Difference from './modules/difference';
 import Form from './modules/forms';
+import ShowInfo from './modules/showInfo';
+import Download from './modules/download';
 
 window.addEventListener('DOMContentLoaded', () => {
 	const slider = new MainSlider({btns: '.next', container: '.page'});
@@ -12,35 +14,39 @@ window.addEventListener('DOMContentLoaded', () => {
 	modulePageSlider.render();
 
 	const showUpSlider = new MiniSlider({
-		container: '.showup__content-slider', 
-		prev: '.showup__prev',
-		next: '.showup__next',
-		activeClass: 'card-active',
-		animate: true
+		 container: '.showup__content-slider',
+		 prev: '.showup__prev',
+		 next: '.showup__next',
+		 activeClass: 'card-active',
+		 animate: true
 	});
 	showUpSlider.init();
 
 	const modulesSlider = new MiniSlider({
-		container: '.modules__content-slider', 
-		prev: '.modules__info-btns .slick-prev',
-		next: '.modules__info-btns .slick-next',
-		activeClass: 'card-active',
-		animate: true,
-		autoplay: true
+		 container: '.modules__content-slider',
+		 prev: '.modules__info-btns .slick-prev',
+		 next: '.modules__info-btns .slick-next',
+		 activeClass: 'card-active',
+		 animate: true,
+		 autoplay: true
 	});
 	modulesSlider.init();
 
 	const feedSlider = new MiniSlider({
-		container: '.feed__slider', 
-		prev: '.feed__slider .slick-prev',
-		next: '.feed__slider .slick-next',
-		activeClass: 'feed__item-active'
+		 container: '.feed__slider',
+		 prev: '.feed__slider .slick-prev',
+		 next: '.feed__slider .slick-next',
+		 activeClass: 'feed__item-active'
 	});
 	feedSlider.init();
 
-	const player = new VideoPlayer('.showup .play', '.overlay');
-	player.init();
+	new VideoPlayer('.showup .play', '.overlay').init();
+	new VideoPlayer('.module__video-item .play', '.overlay').init();
 
 	new Difference('.officerold', '.officernew', '.officer__card-item').init();
 	new Form('.form').init();
+
+	new ShowInfo('.plus__content').init();
+
+	new Download('.download').init();
 });
